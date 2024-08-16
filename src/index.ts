64d5b11d612d6b2be5b066570edf6e856b92e723
Roman Dogoda <piterom911@gmail.com>
2024-08-16 08:16:14 +0200

    Add mock data: addresses and products

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, {Request, Response} from'express'
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
-app.get('/', (req: Request, res: Response) => {
-    let message = "hello message!"
-    res.send(message)
+const products = [{title: "tomato"}, {title: "orange"}]
+const addresses = [{value: "Hüttenberg 6"}, {value: "Schillerstr. 25"}]
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
 })
 
 app.listen(port, () => {
