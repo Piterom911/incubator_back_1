ebef0b3fc4432bb1dd0eb5bd52a62e0ef99caae0
Roman Dogoda <piterom911@gmail.com>
2024-08-16 16:09:18 +0200

    Implement GET method with URI and query parameters

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
-import express, {Request, Response} from'express'
+import express, { Request, Response } from 'express'
 const app = express()
 const port = process.env.port || 3000
 
-const products = [{title: "tomato"}, {title: "orange"}]
-const addresses = [{value: "Hüttenberg 6"}, {value: "Schillerstr. 25"}]
+const products = [{ id: 1, title: "tomato" }, { id: 2, title: "orange", delivery: "bayjin" }]
+const addresses = [{ id: 1, value: "Hüttenberg 6" }, { id: 2, value: "Schillerstr. 25" }]
 
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products)
+  if (req.query.title) {
+    let query = req.query.title.toString()
+    res.send(products.filter(p => p.title.indexOf(query) > -1))
+  }
+  res.send(products)
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
-    res.send(addresses)
+  res.send(addresses)
+})
+
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+  let productTitle = req.params.productTitle
+  let product = products.find(p => p.title === productTitle)
+  if (!product) {
+    res.sendStatus(404)
+  } else {
+    res.send(product)
+  }
+})
+
+app.get('/addresses/:id', (req: Request, res: Response) => {
+  let addressId = +req.params.id
+  let id = addresses.find(a => a.id === addressId)
+  if (!id) {
+    res.sendStatus(404)
+  } else {
+    res.send(id)
+  }
 })
 
 app.listen(port, () => {
